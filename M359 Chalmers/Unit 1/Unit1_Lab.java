053fafd07ec875a9c76bb6b587f27cd7b18ee906
JackChalmers <Chalmers12345@gmail.com>
2016-09-23 16:04:33 -0500

    Update Unit1_Lab.java

--- M359 Chalmers/Unit 1/Unit1_Lab.java
+++ M359 Chalmers/Unit 1/Unit1_Lab.java
@@ -13,17 +13,24 @@ public class Unit1_Lab
         String clubs;
         
         Scanner scan = new Scanner(System.in);
+        
         System.out.println("\fWelcome to a short Personal Interest Survey");
         System.out.println("\nWhat is  your Name?\t");
         name = scan.nextLine();
         System.out.println("\nWhat is your favorite food?\t");
         food = scan.nextLine();
+        System.out.println("\nDo you belong to any clubs?\t");
+        clubs = scan.nextLine();
+        System.out.println("\nWhat sports do you play?\t");
+        sport = scan.nextLine();
         
         int num;
         System.out.print("fav num?\t");
         num = scan.nextInt();
         System.out.print("\nReally? I love " +num + " too!");
     }
+    
+    public 
       
     public static void helper()
     {
